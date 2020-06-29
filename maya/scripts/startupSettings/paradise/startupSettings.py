65a34a20ecc3e15d04915392639754f5e5f3c71c
GabrielCGI <49843136+GabrielCGI@users.noreply.github.com>
2020-06-29 14:08:34 +0200

    turtle delete

--- maya/scripts/startupSettings/paradise/startupSettings.py
+++ maya/scripts/startupSettings/paradise/startupSettings.py
@@ -1,4 +1,30 @@
 import maya.cmds as cmds
+import pymel.core as pm
+ 
+def killTurtle():
+    try:
+        pm.lockNode( 'TurtleDefaultBakeLayer', lock=False )
+        pm.delete('TurtleDefaultBakeLayer')
+    except:
+        pass
+    try:
+        pm.lockNode( 'TurtleBakeLayerManager', lock=False )
+        pm.delete('TurtleBakeLayerManager')
+    except:
+        pass
+    try:
+        pm.lockNode( 'TurtleRenderOptions', lock=False )
+        pm.delete('TurtleRenderOptions')
+    except:
+        pass
+    try:
+        pm.lockNode( 'TurtleUIOptions', lock=False )
+        pm.delete('TurtleUIOptions')
+    except:
+        pass
+    pm.unloadPlugin("Turtle.mll")
+    print "Turtle Killed"
+
 
 def run():
 	#Unit
@@ -6,3 +32,4 @@ def run():
 	print "FPS = 24FPS"
 	cmds.currentUnit( linear='cm' )
 	print "UNIT = cm"
+	killTurtle()
