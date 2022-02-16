6fa730500e8f1c87a94a07b95cebfdad312ec275
GabrielCGI <49843136+GabrielCGI@users.noreply.github.com>
2022-02-16 14:06:12 +0100

    tx pipe

--- maya/scripts/tools/replace_by_tx.py
+++ maya/scripts/tools/replace_by_tx.py
@@ -3,17 +3,49 @@ import maya.cmds as cmds
 def replace_by_tx():
     counter = 0
     errorcounter= 0
+    goodList = []
     allFileNodes = cmds.ls(et="file")
-
     for eachFile in allFileNodes:
         my_file = cmds.getAttr("%s.fileTextureName" % eachFile)
         base = os.path.splitext(my_file)[0]
         new_file = base+".tx"
         if os.path.isfile(new_file):
             counter+=1
+            goodList.append(new_file)
+            cmds.setAttr("%s.ignoreColorSpaceFileRules" % eachFile,1)
             cmds.setAttr("%s.fileTextureName" % eachFile, new_file, type="string")
         else:
            errorcounter+=1
            error =("MISSING TX: %s " % my_file)
            cmds.warning( error )
     print ("Replaced %s files !  \n%s errors"%(counter,errorcounter))
+    for i in goodList:
+        print ("Succed Replaced:"+i)
+
+def replace_by_original():
+    counter = 0
+    errorcounter= 0
+    goodList=[]
+    allFileNodes = cmds.ls(et="file")
+    fileType = ["exr","jpg","png","tiff"]
+    for eachFile in allFileNodes:
+        my_file = cmds.getAttr("%s.fileTextureName" % eachFile)
+        base = os.path.splitext(my_file)[0]
+        checkcounter = counter
+        for type in fileType:
+            new_file = base+"."+type
+
+            if os.path.isfile(new_file):
+                counter+=1
+                goodList.append(new_file)
+                cmds.setAttr("%s.ignoreColorSpaceFileRules" % eachFile,1)
+                cmds.setAttr("%s.fileTextureName" % eachFile, new_file, type="string")
+        if checkcounter == counter:
+               errorcounter+=1
+               error =("MISSING ORIGINAL FILE : %s " % my_file)
+               cmds.warning( error )
+    print ("Replaced %s files !  \n%s errors"%(counter,errorcounter))
+    for i in goodList:
+        print ("Succed Replaced:"+i)
+replace_by_tx()
+replace_by_original()
