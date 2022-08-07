46f6f227844ac3b9f4ea219b7c19ea0c3691f4cb
GabrielCGI <49843136+GabrielCGI@users.noreply.github.com>
2022-08-07 17:07:39 +0200

    add my_comment = "ranch_path_mapping" as controler

--- maya/scripts/deadline/JobPreLoad.py
+++ maya/scripts/deadline/JobPreLoad.py
@@ -8,6 +8,7 @@ import os
 
 def __main__(deadlinePlugin):
     job = deadlinePlugin.GetJob()
+    my_comment = job.Comment
     deadlinePlugin.LogInfo("Setting up env variable for Illogic path mapping. ")
     DISK_I_on_ranch = "//RANCH-159/guerlain_cache/I"
     DISK_R_on_ranch = "//RANCH-159/guerlain_cache/R"
@@ -15,7 +16,8 @@ def __main__(deadlinePlugin):
     DISK_I_on_local = "I:"
     DISK_R_on_local = "R:"
 
-    if os.environ['COMPUTERNAME'].startswith('RANCH'):
+    #APPLY ENV VARRIABLE FOR PATH MAMMINF IF IT's A RANCH AND IF THE COMMENT = RANCH_PATH_MAPPING
+    if os.environ['COMPUTERNAME'].startswith('RANCH') and  my_comment == "ranch_path_mapping":
         deadlinePlugin.SetProcessEnvironmentVariable("DISK_I", DISK_I_on_ranch)
         deadlinePlugin.SetProcessEnvironmentVariable("DISK_R", DISK_R_on_ranch )
         deadlinePlugin.LogInfo("Setting DISK_I="+DISK_I_on_ranch)
