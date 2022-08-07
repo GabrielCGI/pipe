825d7d6f6b60cc105e986edf764fba7fe46ca635
GabrielCGI <49843136+GabrielCGI@users.noreply.github.com>
2022-08-07 20:00:31 +0200

    submitToDealine custom (add comment used as variable)

--- maya/scripts/tools/submitToDeadline_pathMapping.py
+++ maya/scripts/tools/submitToDeadline_pathMapping.py
@@ -2,6 +2,7 @@ import importlib
 import collectMayaScene
 importlib.reload(collectMayaScene)
 import replace_by_tx
+import maya.mel as mel
 
 
 def replace_abc_pipeline_script_node():
@@ -12,22 +13,24 @@ def replace_abc_pipeline_script_node():
     for script in script_list:
         if script.startswith('scriptNode_ch'):
             pipeline_script.append(script)
-            
+
     for abcScript in pipeline_script:
-        counter_node+=1
+        counter_nodes+=1
         abcCommand = (cmds.scriptNode(abcScript, query=True, bs=True))
         abc_modif=abcCommand.replace("I:/","$DISK_I/")
         if abcCommand != abc_modif:
             cmds.scriptNode(abcScript , edit =True, bs=abc_modif)
             counter_udpate += 1
-    print (str(counter_nodes)+" abc pipeline node found")        
+    print (str(counter_nodes)+" abc pipeline node found")
     print (str(counter_udpate) +" abc pipeline node modified !")
-    
+
 print ("Start replacing by tx !")
 replace_by_tx.replace_by_tx()
 
-print 
-collectMayaScene.run()
-
 print (" \nStart Abc pipeline script node path mapping")
 replace_abc_pipeline_script_node()
+
+#increment and save
+mel.eval('incrementAndSaveScene 0;')
+print
+collectMayaScene.run()
