d6fe20f202d90bd87a40cc08ff1ddc099b90d4b7
GabrielCGI <49843136+GabrielCGI@users.noreply.github.com>
2022-12-07 15:32:51 +0100

    pymel

--- maya/scripts/assetier_pymel/proxy_prepper_pymel.py
+++ maya/scripts/assetier_pymel/proxy_prepper_pymel.py
@@ -1,186 +1,47 @@
 import os
 import maya.cmds as cmds
-#TO DO create light no shadow?
-import utils as utils
 import importlib
-importlib.reload(utils)
 import logging
+import pymel.core as pm
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
-def bake_texture(obj, dir,resolution=1024):
-    if not obj.endswith("_proxy"):
-        utils.warning("The selected object must be a proxy (ex: rock_proxy)")
-    uv_generate = False
-    auto_layout_uv = False
-
-    result = cmds.confirmDialog( title='UV?',
-                        message='What about uvs?',
-                        button=['Auto-generate','Use existing','Existing + Autolayout'],
-                        defaultButton='Use existing',
-                        cancelButton='"Cancel',
-                        dismissString='"Cancel' )
-    if result == "Auto-generate":
-        uv_generate = True
-
-    if result == "Use existing":
-        uv_generate = False
-    if result == "Existing + Autolayout":
-        auto_layout_uv = True
-
-    if result == "Cancel":
-        utils.warning("abort by user")
-
-    #Lights manipulation
-    dic_lights={}
-    lights = cmds.ls(type="light")
-    for l in lights:
-        dic_lights[l]= cmds.getAttr(l+".visibility")
-        cmds.setAttr(l+".visibility",0)
-    domeL = cmds.createNode("aiSkyDomeLight")
-    transform_domeL= cmds.listRelatives(domeL,parent=True)[0]
-    cmds.connectAttr(transform_domeL+".instObjGroups", "defaultLightSet.dagSetMembers",nextAvailable=True)
-    cmds.setAttr(domeL+".aiCastShadows",1)
-    cmds.setAttr(domeL+".aiSpecular",0)
 
 
-    default_uv  = merge_uv_sets(obj)
+zero_matrix = [1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0]
+def warning(txt):
+    pm.error(txt)
 
+def only_name(obj):
+    only_name = obj.name("|")[-1]
+    return only_name
 
-    cmds.select(obj)
-    proxy_uvset = default_uv
-    if uv_generate:
-        proxy_uvset = cmds.polyUVSet(create=True,uvSet = "proxyUv")[0]
-        cmds.polyAutoProjection(uvSetName=proxy_uvset,ps= 0.4)
-
-    if auto_layout_uv:
-        proxy_uvset = cmds.polyUVSet(create=True,uvSet = "proxyUv")[0]
-        cmds.polyCopyUV(obj, uvi=default_uv , uvs=proxy_uvset)
-        cmds.polyUVSet(obj,currentUVSet=True,uvSet=proxy_uvset)
-        cmds.u3dLayout(res = 512, scl=1, rmn = 0, rmx = 360, rst= 25, spc= 0.003, mar = 0.003)
-
-    cmds.select(obj)
-    cmds.move(0,1000,0,relative=True)
-    cmds.arnoldRenderToTexture(folder=dir,aa_samples=2,extend_edges=True,resolution=resolution, enable_aovs=True,uv_set=proxy_uvset)
-    cmds.move(0,-1000,0,relative=True)
-    shape = cmds.listRelatives(obj, shapes=True)[0]
-    albedo = [f for f in os.listdir(dir) if f.endswith(".exr") and shape in f][0]
-    albedo_path = os.path.join(dir,albedo)
-    file_node_albedo = cmds.shadingNode("file", asTexture=True, name ="proxyAlbedo")
-    cmds.setAttr(file_node_albedo+".fileTextureName",albedo_path, type="string")
-    cmds.setAttr(file_node_albedo+".cs","ACEScg", type="string")
-
-    proxy_shader = cmds.shadingNode("aiStandardSurface", asShader=True)
-    #TO DO rename
-    cmds.setAttr(proxy_shader+".specular",0.4)
-    shadingGroup = cmds.sets(name="%sSG" % proxy_shader, empty=True, renderable=True, noSurfaceShader=True)
-    cmds.connectAttr(file_node_albedo+".outColor ", proxy_shader +".baseColor")
-    cmds.connectAttr("%s.outColor" % proxy_shader, "%s.surfaceShader" % shadingGroup)
-    cmds.sets(obj, e=True, forceElement= shadingGroup)
-    cmds.select(obj)
-    cmds.polyCopyUV(obj, uvi=proxy_uvset,uvs=default_uv )
-    cmds.delete(obj, constructionHistory = True)
-    default_uv  = merge_uv_sets(obj)
-    cmds.delete(obj, constructionHistory = True)
-    
-    #Clean Lights
-    cmds.delete(transform_domeL)
-    for l in dic_lights.keys():
-        cmds.setAttr(l+".visibility",dic_lights[l])
-    logger.info("Bake textures success ! ")
-
-def merge_uv_sets(obj):
-    default_uv = cmds.getAttr(obj+".uvSet[0].uvSetName")
-    all_uv_sets = cmds.polyUVSet(obj, q=1, allUVSets=1)
-    all_uv_sets.remove(default_uv)
-    #temp_uv  = cmds.polyUVSet(create=True,uvSet = "temp_uv")[0]
-    for uv_set in all_uv_sets:
-        cmds.polyUVSet(currentUVSet = True, uvSet=uv_set)
-        uvs = cmds.polyListComponentConversion(obj, toUV=True)
-        #cmds.select(uvs)
-        cmds.polyCopyUV( uvs, uvi= uv_set, uvs=default_uv )
-        cmds.polyUVSet( delete=True, uvSet=uv_set)
-    return default_uv
-
-def generate_lowpoly(HD_grp):
-    SD_GRP = cmds.duplicate(HD_grp,"SD")[0]
-    SD_GRP = cmds.rename(SD_GRP,"SD")
-    logger.info("SD group created")
-    shapes = cmds.ls(SD_GRP,dag=1,o=1,s=1,sl=1)
-    displace_disable(shapes)
-    logger.info("Disable displace succes")
-    catclark_max(shapes,1)
-
-def displace_disable(shapes):
-    shaderGroup=[]
-    list_shadingGrp = []
-    for shape in shapes:
-        shadingGrp = cmds.listConnections(shape,type='shadingEngine')
-        if shadingGrp:
-            if len(shadingGrp)==1:
-                if shadingGrp[0] != 'initialShadingGroup':
-                    if shadingGrp[0] not in list_shadingGrp:
-                        list_shadingGrp.append(shadingGrp[0])
-            else:
-                print ("Too many shading group: %s"%str(len(shadingGrp)))
-
-
-    counter= 0
-    for shadingGrp in list_shadingGrp:
-        displace_shader = cmds.listConnections(shadingGrp+".displacementShader")
-        if displace_shader == None:
-            continue
-        shader = cmds.listConnections(shadingGrp+".surfaceShader")
-        shader_ai = cmds.listConnections(shadingGrp+".aiSurfaceShader")
-
-        newshadingGroup = cmds.sets(name="lowpoly_%s" % shadingGrp, empty=True, renderable=True, noSurfaceShader=True)
-        if shader_ai != None:
-            cmds.connectAttr("%s.outColor" % shader_ai[0], "%s.aiSurfaceShader" % newshadingGroup)
-        if shader != None:
-            cmds.connectAttr("%s.outColor" % shader[0], "%s.surfaceShader" % newshadingGroup)
-            member_list = cmds.sets(shadingGrp, q=True)
-        if shader != None or shader_ai != None:
-            #Assign only to the geo that is both part of lowpoly set and shading group set
-            match_set = set(member_list).intersection(shapes)
-            cmds.sets(list(match_set), e=True, forceElement=newshadingGroup)
-            logger.info("Shading group created: %s"%newshadingGroup)
-            logger.info("Shape list: " + ",".join(list(match_set)))
-            counter+=1
-    logger.info("%s shading group replaced"%(counter))
-
-def getsize(sel):
-    if not sel:
-        return 0
-    try:
-        bb = cmds.xform(sel,bb=True, query=True)
-        x = bb[3]-bb[0]
-        y = bb[4]-bb[1]
-        z = bb[5]-bb[2]
-        moy = (x+y+z)/3
-        edge_length = moy *0.05
-        return edge_length
-    except Exception as e:
-        print(e)
-        return 0
-
-def build_hiearchy():
-    logger.info("Start building hiearchy...")
-    sel = cmds.ls(selection=True)
-    if len(sel)!=1:
-        utils.warning("Please select only one object (group or geo)")
-    cmds.select(sel) #HACK TO PREVENT "FALSE SELECTION" AFTER MANUALY CREATING A GROUP WITH CTRL+G
-    logger.info("Selection: %s"%sel)
-    scene_path = cmds.file(q=True, sn=True)
-    if "shots" in scene_path:
-        asset_name = sel.split("|")[-1]
-        logger.info("'Shot' has been detected in the scene path")
+def lock_all_transforms(obj):
+    if type(obj) == list:
+        for o in obj:
+            o.translate.lock(True)
+            o.rotate.lock(True)
+            o.scale.lock(True)
     else:
-        asset_name =  os.path.basename(scene_path).split("_")[0].split(".")[0]
-        logger.info("Asset name: %s"%asset_name)
+        obj.translate.lock(True)
+        obj.rotate.lock(True)
+        obj.scale.lock(True)
 
 
-    scene_parent = cmds.listRelatives(sel,parent=True,fullPath=True)
-    logger.info("Scene parent: %s"%scene_parent)
+def build_hiearchy(obj):
+    #Check
+    if len(obj) != 1:
+        warning("Too many object selected")
+
+    #Get asset name
+    scene_path = pm.system.sceneName()
+    if "shots" in scene_path:
+        asset_name = only_name(obj)
+        logger.info("Current scene is detected as: shot")
+    else:
+        asset_name = os.path.basename(scene_path).split("_")[0].split(".")[0]
+        logger.info("Current scene is detected as: asset")
+    """    
     result = cmds.promptDialog(
                     title='Rename Object',
                     message='Enter Name:',
@@ -192,101 +53,192 @@ def build_hiearchy():
     if result == "OK":
         asset_name = cmds.promptDialog(query=True, text=True)
     else:
-        cmds.warning('abort by user')
-        raise
+        warning('abort by user')
+    """
+    asset_name = "test"
+    scene_parent = pm.listRelatives(obj,parent=True)
 
-    if len(cmds.listRelatives( sel, children=True ))>1:
-        logger.info("'Renaming group %s to geo"%sel)
-        sel = cmds.rename(sel,"geo")
+    if pm.objExists("|"+asset_name):
+        pm.rename("|"+asset_name,"|"+asset_name+"_1")    
+    asset_grp = pm.createNode("transform", n=asset_name)
+    pm.matchTransform(asset_grp, obj)
+    pm.parent(obj,asset_grp)
+    pm.makeIdentity(obj, apply=True)
+    pm.delete(obj, constructionHistory=True)
+    pm.rename(obj, "HD")
+    print(obj)
+    lock_all_transforms(obj)
+    
 
-    else:
-        if sel == asset_name:
-            logger.info("Adding geo suffix to %s"%sel)
-            sel = cmds.rename(sel,sel+"_geo")
-
-
-    g = cmds.createNode("transform")
-    g1 = cmds.createNode("transform")
-    g2 = cmds.createNode("transform")
-    g3 = cmds.createNode("transform")
-    logger.info("Locking transform.")
-    utils.lock_all_transform(g2)
-    utils.lock_all_transform(g3)
-
-    asset_grp =cmds.parent(g1,g)[0]
-    basic_grp = cmds.parent(g2,asset_grp)[0]
-    hd_grp  = cmds.parent(g3,basic_grp)[0]
-
-    cmds.rename(hd_grp,"HD")
-    cmds.rename(basic_grp,"variant_basic")
     if scene_parent:
-        asset_grp = cmds.parent(asset_grp,scene_parent)
-    else:
-        asset_grp = cmds.parent(asset_grp,world=True)
-    cmds.delete(g)
-    asset_grp = cmds.rename(asset_grp,asset_name)
+        pm.parent(asset_grp,scene_parent)
 
-    m = cmds.xform(sel, relative = True, matrix=True, query=True)
-    cmds.xform(asset_grp, matrix=m)
+    return asset_grp, obj  
+    
 
-    hd_grp = asset_grp+"|variant_basic|HD"
-    logger.info("HD group: %s"%hd_grp)
-    sel = cmds.parent(sel,hd_grp)[0]
-    logger.info("Parenting: %s to %s"%(sel,hd_grp ))
-    logger.info("Hierarchy building is a succes !")
+def generate_lowpoly(hd_grp):
+    sd_grp = pm.duplicate(hd_grp,name="SD")
 
+    sd_grp=sd_grp[0]
+    shapes = pm.listRelatives(sd_grp,allDescendents=True,shapes=True)
+    disable_displace(shapes)
+    catclark_max(shapes)
+    return sd_grp
 
+def disable_displace(shapes):
+    list_sg = []
+    for shape in shapes:
+        sg = pm.listConnections(shape,type="shadingEngine", destination=True)     
+        if len(sg) == 1:
+            if sg[0].name() != "initialShadingGroup":
+                if sg[0] not in list_sg:
+                    list_sg.append(sg[0])
 
-    #zero_matrix = [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1]
-    #cmds.xform(sel, matrix=zero_matrix)
+    for sg in list_sg:
 
-def catclark_max(objs,max=1):
-    for obj in objs:
+        displace = pm.listConnections(sg+".displacementShader")
+        if not displace:
+            continue
+
+        shader =  pm.listConnections(sg+".surfaceShader")
+        shader_ai =  pm.listConnections(sg+".aiSurfaceShader")
+
+        new_sg = pm.sets (name= "sd_"+sg.name(), empty=True, renderable=True, noSurfaceShader=True)
+        if shader_ai:
+            pm.connectAttr(shader_ai[0]+".outColor", new_sg+".aiSurfaceShader")
+
+        if shader:
+            pm.connectAttr(shader[0]+".outColor", new_sg+".surfaceShader")
+        member_list = sg.members()
+
+        if shader or shader_ai:
+            match_set = set(member_list).intersection(shapes)
+            new_sg.forceElement(match_set)
+
+def catclark_max(shapes, sub_max=1):
+    for s in shapes:
         try:
-            type = cmds.getAttr(obj+".aiSubdivType")
+            subdiv_type = pm.getAttr(s+".aiSubdivType")
+            ite = pm.getAttr(s+".aiSubdivIterations")
 
-            ite = cmds.getAttr(obj+".aiSubdivIterations")
-
-            if type ==1 and ite > max:
-                cmds.setAttr(obj+".aiSubdivIterations",max)
+            if subdiv_type == 1 and ite >sub_max:
+                logger.debug("Lowering catclark on %s: %s to %s"%(s, ite,sub_max))
+                pm.setAttr(s+".aiSubdivIterations",max)
         except Exception as e:
             print(e)
 
-def proxy_generate(asset_name,asset_root,obj_root,name,targetVertex= 3000):
-    childrens = cmds.listRelatives(obj_root,allDescendents=True,fullPath=True)
+def merge_uv_sets(obj):
+    default_uv = pm.getAttr(obj+".uvSet[0].uvSetName")
+    all_uv_sets = pm.polyUVSet(obj, q=1, allUVSets=1)
+    all_uv_sets.remove(default_uv)
 
-    if len(childrens) == 2: #SHAPE + TRANSFROMS
-        obj_root = childrens[0]
-    proxy= cmds.duplicate(obj_root, name=name)
-    proxy = proxy[0]
-    logger.debug("Duplicate: %s --> %s"%(str(obj_root),proxy))
-    childrens = cmds.listRelatives(proxy,allDescendents=True, type="mesh",fullPath=True)
-    if cmds.listRelatives(proxy,parent=True) != None:
-        proxy = cmds.parent(proxy, world=True )[0]
+    for uv_set in all_uv_sets:
+        pm.polyUVSet(currentUVSet = True, uvSet=uv_set)
+        uvs = pm.polyListComponentConversion(obj, toUV=True)
+        pm.polyCopyUV( uvs, uvi= uv_set, uvs=default_uv )
+        pm.polyUVSet( delete=True, uvSet=uv_set)
+    return default_uv
+
+def bake_texture(obj,dir,resolution=512):
+    if not obj.endswith("_proxy"):
+        warning("Not a proxy")
+
+    result = pm.confirmDialog( title='UV?',
+                        message='What about uvs?',
+                        button=['Auto-generate','Use existing','Existing + Autolayout'],
+                        defaultButton='Use existing',
+                        cancelButton='"Cancel',
+                        dismissString='"Cancel' )
+
+    if result == "Cancel":
+        warning("abort by user")
+
+    default_uv  = merge_uv_sets(obj)
+    
+    if result == "Auto-generate":
+        proxy_uvset = pm.polyUVSet(create=True,uvSet = "proxyUv")[0]
+        pm.polyAutoProjection(uvSetName=proxy_uvset,ps= 0.4)
+
+    if result == "Existing + Autolayout":
+        proxy_uvset = pm.polyUVSet(create=True,uvSet = "proxyUv")[0]
+        pm.polyCopyUV(obj, uvi=default_uv , uvs=proxy_uvset)
+        pm.polyUVSet(obj,currentUVSet=True,uvSet=proxy_uvset)
+        pm.u3dLayout(res = 512, scl=1, rmn = 0, rmx = 360, rst= 25, spc= 0.003, mar = 0.003)
+
+    if result == "Use existing":
+        proxy_uvset = default_uv
+
+    
+
+    dic_lights={}
+    lights = pm.ls(type=["light","aiSkyDomeLight","aiAreaLight"])
+    for l in lights:
+        l= l.getTransform()
+        dic_lights[l]= l.visibility
+        l.visibility.set(False)
+    domeL =  pm.createNode("aiSkyDomeLight")
+    pm.connectAttr(domeL.getTransform()+".instObjGroups", "defaultLightSet.dagSetMembers",nextAvailable=True) #Illuminate by default
+    domeL.aiSpecular.set(0)
+
+    pm.move(obj,1000,r=True)
+    pm.select(obj)
+    pm.arnoldRenderToTexture(   
+                                folder=dir,
+                                aa_samples=2,extend_edges=True,
+                                resolution=resolution,
+                                enable_aovs=True,
+                                uv_set=proxy_uvset
+                            )
+    pm.move(obj,-1000,r=True)
+
+    
+    albedo = [f for f in os.listdir(dir) if f.endswith(".exr") and obj.name() in f][0]
+    albedo_path = os.path.join(dir,albedo)
+    file_node_albedo = pm.shadingNode("file", asTexture=True, name ="proxyAlbedo")
+    file_node_albedo.fileTextureName.set(albedo_path)
+    file_node_albedo.cs.set("ACEScg")
+
+    proxy_shader = pm.shadingNode("aiStandardSurface", asShader=True, name=obj.name()+"_shader")
+    proxy_shader.specular.set(0.4)
+
+    shadingGroup = pm.sets(name="%sSG" % proxy_shader, empty=True, renderable=True, noSurfaceShader=True)
+    pm.connectAttr(file_node_albedo+".outColor ", proxy_shader +".baseColor")
+    pm.connectAttr("%s.outColor" % proxy_shader, "%s.surfaceShader" % shadingGroup)
+    shadingGroup.forceElement(obj)
+
+    pm.polyCopyUV(obj, uvi=proxy_uvset,uvs=default_uv )
+    default_uv  = merge_uv_sets(obj)
+    pm.delete(obj, constructionHistory = True)
+    pm.delete(domeL.getTransform())
+
+    for l in dic_lights.keys():
+        l.visibility.set(True)
+    logger.info("Bake textures success ! ")
+
+def generate_proxy(grp, target_vertex=500):
+    proxy = pm.duplicate(grp)
+    proxy_parent = pm.listRelatives(proxy,parent=True)
+
     try:
-        unit = cmds.polyUnite (proxy,mergeUVSets=True)[0]
-        cmds.delete(unit , constructionHistory = True)
-        proxy = cmds.rename(unit,name)
+        proxy =pm.polyUnite(proxy, mergeUVSets=True)[0]
+        pm.delete(proxy,constructionHistory=True)
+        logger.info("Poly Unit success %s"%proxy)
+    except Exception as e:
+        logger.info("Skipping poly unit %s"%proxy)
+        pass
+    pm.polyReduce(proxy,ver = 1,trm=1, keepQuadsWeight=0,vct=500 )
+    pm.parent(proxy,proxy_parent)
+    pm.makeIdentity(proxy, apply=True )
+    pm.matchTransform(proxy,proxy_parent, pivots=True)
+    pm.rename(proxy, proxy_parent[0]+"_proxy")
+    pm.delete(proxy,constructionHistory=True)
+    
+obj = pm.ls(selection=True)[0]
 
-    except:
-        logger.info("Only one object, skipping polyUnite step ")
+#asset_grp, hd_grp = build_hiearchy(obj)
+#sd_grp = generate_lowpoly(hd_grp)
 
-    cmds.select(proxy)
+#generate_proxy(sd_grp)
 
-    cmds.polyReduce (ver=1 ,trm=1 ,shp=0, keepBorder=1 ,keepMapBorder=1 ,
-                    keepColorBorder=1 ,keepFaceGroupBorder=1 ,keepHardEdge=1 ,
-                    keepCreaseEdge=1 ,keepBorderWeight=0.5 ,
-                    keepMapBorderWeight=0.5,keepColorBorderWeight=0.5,
-                    keepFaceGroupBorderWeight=0.5,
-                    keepHardEdgeWeight=0.5 , keepCreaseEdgeWeight=0.5,
-                    useVirtualSymmetry=0,preserveTopology=1,keepQuadsWeight=0,
-                    cachingReduce=1 ,ch=1 ,p=50 ,vct=targetVertex ,tct=0 ,replaceOriginal=1)
-    logger.info("Poly reducing success! (%s vertex)"%targetVertex)
-    cmds.delete(proxy, constructionHistory = True)
-    proxy = cmds.parent(proxy,asset_root)[0]
-    zero_matrix = [1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0]
-    cmds.xform(proxy,  matrix=zero_matrix)
-
-    cmds.select(proxy)
-    logger.info("Proxy generate success ! ")
-    return (proxy)
+#merge_uv_sets(obj)
+bake_texture(obj,"D:/")
