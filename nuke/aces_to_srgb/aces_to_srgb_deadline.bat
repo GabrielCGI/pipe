77dc439846d2a41c85be311110500b674a17d623
GabrielCGI <49843136+GabrielCGI@users.noreply.github.com>
2023-05-17 16:17:11 +0200

    sRGB

--- nuke/aces_to_srgb/aces_to_srgb_deadline.bat
+++ nuke/aces_to_srgb/aces_to_srgb_deadline.bat
@@ -5,4 +5,4 @@ set folder=%folder:\=/%
 set folder=%folder:"=%
 set command="folder=\"%folder%\""
 
-start R:\pipeline\networkInstall\natron\2.5.0\Natron\bin\NatronRenderer.exe %~dp0/aces_to_srgb.py -c %command%
+start C:\"Program Files"\Natron\bin\NatronRenderer.exe %~dp0/aces_to_srgb.py -c %command%
