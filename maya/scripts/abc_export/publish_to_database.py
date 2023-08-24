b66333a3f7456f05e26d7f2cb74130aeca69c5f1
GabrielCGI <49843136+GabrielCGI@users.noreply.github.com>
2023-08-24 17:28:07 +0200

    database

--- maya/scripts/abc_export/publish_to_database.py
+++ maya/scripts/abc_export/publish_to_database.py
@@ -18,9 +18,11 @@ import projects as projects
 import importlib
 importlib.reload(projects)
 
+
+
 # Project variable initialisation
-assetsDbDir = projects.getCurrentProjectData()["assetsDbDir"] #Something like B:\Teaser\assets\database #HACK paradise usually
-assetsDir = projects.getCurrentProjectData()["assetsDir"] #Something like B:\Teaser\assets\database
+assetsDbDir = os.getenv("CURRENT_PROJECT_DIR")+"/assets/_database"#Something like B:\Teaser\assets\database #HACK paradise usually
+assetsDir = os.getenv("CURRENT_PROJECT_DIR")+"/assets" #Something like B:\Teaser\assets
 
 def mayaWarning(msg):
     """
@@ -29,51 +31,12 @@ def mayaWarning(msg):
     cmds.warning(msg)
     cmds.confirmDialog(title="Error",message=msg)
 
-def buildCommand (abcPath, geoList, attrList):
-   "Build Command for alembic export job"
-   command = ""
-   command += "-frameRange 1 1" #Frame Range
-   command += " -writeVisibility -uvWrite -writeUVSets -dataFormat ogawa "
-   if attrList[0]:
-       for attr in attrList:
-           command += " -attr %s"%(attr)           #Attributs
-   for geo in geoList:
-       geoRootName = cmds.ls(geo, l=True)[0]     #Geo List
-       command += " -root %s"%geoRootName
 
-   command += " -file %s"%(abcPath)                #File Path
-   return command
-
-def exportAbc(abcPath, currentSelection, attrList):
-    """Export an alembic of current selection.
-       Return the path of the created alembic"""
-    geoList = currentSelection
-    # Add custom attribut to export with alembic.
-    command = buildCommand(abcPath,geoList,attrList)
-    print("------------------------\n------------ BEGGINING EXPORT %s ------------ \n %s"%(abcPath, command))
-    cmds.refresh(suspend=True)
-    cmds.AbcExport (j= command )
-    cmds.refresh(suspend=False)
-    print("------------ SUCCESS EXPORT ! ------------")
-    return abcPath
 
 def createNewAsset():
     """
     Main function that drive the export
     """
-    #Info window
-    msg = "About this tool.\n\nThis tools add new assets into the database for easy export/import Alembic."
-    msg +="\n\nImportant - Select only the geometry you want to export."
-    msg +="\nThis maya scene need to have a referenced rig lib (Ch_assetName_rig_lib)"
-    msg +="\nExport alembic, write data and automaticaly create the shading scene"
-    confirm = cmds.confirmDialog( title='Abc pipeline Tools',
-                                    message=msg,
-                                    button=['Continue','Stop'], defaultButton='Continue', cancelButton='Stop', dismissString='Stop'                                    )
-    if confirm == "Stop":
-        msg = "Abort by user."
-        mayaWarning(msg)
-        sys.exit(msg)
-    #End info window
 
     #Get current selection
     currentSelection = cmds.ls(selection=True)
@@ -111,42 +74,6 @@ def createNewAsset():
         mayaWarning(msg)
         sys.exit(msg)
 
-    assetAbcDir = os.path.join(assetDir, "abc", )
-    # Create abc Directory if don't exist
-    if not os.path.exists(assetAbcDir):
-        os.mkdir(assetAbcDir)
-        print(("Directory " , assetAbcDir ,  " Created "))
-    else:
-        print(("Directory " , assetAbcDir ,  " already exists"))
-
-    #Check is scene is save.
-    confirm = "Yes"
-    if cmds.file(q=True,anyModified=True):
-        confirm = cmds.confirmDialog( title='Shading scene creation',
-                                    message='Shading scene creation ready to start... \nThe current scene is not save. Continue ?',
-                                    button=['Yes','No'], defaultButton='Yes', cancelButton='No', dismissString='No'
-                                    )
-    if confirm == "No":
-        msg = "Scene not save. \nAbort by user."
-        mayaWarning(msg)
-        sys.exit(msg)
-    #CHECK IF FILE NOT ALREADY EXIST
-    #Check abc
-    num ="00" #Base number
-    abcName = "%s_%s.abc"%(assetName,num)
-    abcPath = os.path.join(assetAbcDir,abcName)
-    print(abcPath)
-    if os.path.isfile(abcPath):  #Check if the abc file doesn't already exist
-        msg = "File already exist! \n%s\nDelete or rename the existing file manually.\nCan't ovewrite, not safe... "%(abcPath)
-        mayaWarning(msg)
-        sys.exit(msg)
-    #Check shading
-    assetShadingPath = os.path.join(assetDir,"%s_shading_lib.mb"%(assetName))
-    if os.path.isfile(assetShadingPath):  #Check if the shading file doesn't already exist
-        msg = "File already exist! \n%s\nDelete or rename the existing file manually.\nCan't ovewrite, not safe... "%(assetShadingPath)
-        mayaWarning(msg)
-        sys.exit(msg)
-    print(assetShadingPath)
     #Check asset data
     assetTxt = assetName+'.txt'
     print("assetDbDir"+assetsDbDir)
@@ -172,10 +99,10 @@ def createNewAsset():
     attrString = cmds.promptDialog(query=True, text=True)
     attrList = attrString.split(",")
 
-    exportAbc(abcPath, currentSelection, attrList) # Export the alembic and get the path
-    createShadingScene(assetShadingPath, assetName, abcPath)
+    assetShadingPath=""
     assetData = buildAssetData(assetName,currentSelection,assetShadingPath, attrList)
     writeAssetData(assetData,assetName,assetsDbDir)
+    cmds.confirmDialog(title="it's worked",message="Succes !")
 
 def buildAssetData(name, currentSelection, assetShadingPath, attrList):
     """
@@ -187,14 +114,9 @@ def buildAssetData(name, currentSelection, assetShadingPath, attrList):
     geoList = currentSelection  #Get the current selected geometry in a list
     rig = currentSelection[0].split(":")[0]  #Get the rig namespace
 
-    assetData = {"name":name,"geo":geoList,"rig":rig,"attr":attrList,"shading":assetShadingPath}
+    assetData = {"name":name,"geo":geoList,"rig":rig,"attr":"","shading":""}
     return assetData
 
-def createShadingScene(assetShadingPath, assetName, abcPath):
-    cmds.file(f=True, new=True)
-    cmds.file( abcPath, r=True, type="Alembic", namespace=assetName)
-    cmds.file( rename=assetShadingPath )
-    cmds.file( save=True, type='mayaBinary')
 
 def writeAssetData(assetData,assetName,assetsDbDir):
     """
@@ -215,3 +137,4 @@ def writeAssetData(assetData,assetName,assetsDbDir):
             raise
 
     return
+createNewAsset()
